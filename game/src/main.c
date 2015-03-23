84f22241a26df31518a65b307c717050f969679e
messiasthi <messiasthi@gmail.com>
2015-03-23 00:55:44 -0300

    Definindo fluxo.

--- game/src/main.c
+++ game/src/main.c
@@ -2,13 +2,39 @@
 
 #define ARQUIVO_CONFIG "../config/configuracao.conf"
 #define ESPACO_MEMORIA 100
+#define FALSE 0
+#define TRUE 1
 
-int main() {
+int main(int argc, char **argv) {
+
 	config *configuracao = ler_arquivo_configuracao(ARQUIVO_CONFIG);
 	char aux[ESPACO_MEMORIA];
 	camera *camera1 = camera_inicializa(0);
+
 	sprintf(aux, "Camera: Altura= %d\nLargura= %d", camera1->altura, camera1->largura);
 	registra(aux);
+
+	int tentativas = 0;
+
+	while(TRUE) {
+		switch menuInicial(tentativas) {
+			case 0:
+				tentativas += iniciarJogo(argv);
+				break;
+
+			case 1:
+				mostrarRanking();
+				break;
+
+			case 2:
+				mostrarTutorial();
+				break;
+
+			default:
+				sair();
+		}
+	}
+
 	camera_finaliza(camera1);
 
 	return 0;
